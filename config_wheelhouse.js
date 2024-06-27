23c014cfd29baad2678d1b3dc1040cda4e629775
AdvisoJessica <jessica@adviso.io>
2024-06-26 18:34:54 -0700

    Update config_wheelhouse.js

--- config_wheelhouse.js
+++ config_wheelhouse.js
@@ -53,7 +53,7 @@ window.botbuilder.init({
     "botName": "Wheelhouse Buddy",
     "avatarUrl": "https://i.imgur.com/MFf8dyk.png",
     "website": "https://clevvur.com",
-    "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/a1dadc2f-b19f-4633-99a4-f5aecb4daacc/v70253/style.css",
+    "stylesheet": "font-size: 14px;",
     "frontendVersion": "v1",
     "enableConversationDeletion": true,
     "theme": "galaxy",
