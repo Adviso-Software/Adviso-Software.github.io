97bd7b558df20bcbe2e33a7d429d737600d8b3fa
Valentin Forestier <81619333+valforest90@users.noreply.github.com>
2024-06-29 08:53:52 +0200

    Update chatbot_inject_react_wheelhouse.js

--- chatbot_inject_react_wheelhouse.js
+++ chatbot_inject_react_wheelhouse.js
@@ -106,7 +106,7 @@
                  <img src="https://i.imgur.com/CYf5wdV.png" alt="Icon" style="height: 60px; width: 60px;" />
              </div>
               
-              <span class="bounce-text" style="bottom:65px; width: 150px; right:10px; background-color: rgba(0,0,0,1); opacity: 0.7; padding:7px; border-radius: 50px; border-bottom-right-radius: 0px; position: absolute;">
+              <span id="bounce-text" style="bottom:65px; width: 150px; right:10px; background-color: rgba(0,0,0,1); opacity: 0.7; padding:7px; border-radius: 50px; border-bottom-right-radius: 0px; position: absolute;">
                 Need help? Talk to me!
               </span>
             `;
@@ -247,7 +247,7 @@
             showChatbotBtn.id = 'show-chatbot-btn';
             showChatbotBtn.innerHTML = `
               <img src="https://i.imgur.com/CYf5wdV.png" alt="Icon" style="height: 128px; width: 128px; margin-right: -25px;" />
-              <span class="bounce-text" style="bottom:130px; width: 200px; right:10px; background-color: rgba(0,0,0,1); opacity: 0.7; padding:10px; border-radius: 50px; border-bottom-right-radius: 0px; position: absolute;">
+              <span id="bounce-text" style="bottom:130px; width: 200px; right:10px; background-color: rgba(0,0,0,1); opacity: 0.7; padding:10px; border-radius: 50px; border-bottom-right-radius: 0px; position: absolute;">
                 Need help? Talk to me!
               </span>
             `;
