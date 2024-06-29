191f7f2a8bb120ad37ab3e7f89292891f752dfe7
Valentin Forestier <81619333+valforest90@users.noreply.github.com>
2024-06-29 08:43:37 +0200

    Update chatbot_inject_react_wheelhouse.js

--- chatbot_inject_react_wheelhouse.js
+++ chatbot_inject_react_wheelhouse.js
@@ -243,7 +243,7 @@
               <img src="https://adviso-software.github.io/icon.jpg" alt="Round" style="width: 50px; height: 50px; background-color: white; border-radius: 500px; padding: 2px; margin-top: 4px; margin-right: 10px;" />
                 <div style="display: flex; flex-direction: column; padding-top: 10px;">
                   <span style="color:white">Wheelhouse Buddy</span>
-                  <button style="text-align: left; margin-top: -5px; text-decoration: underline; background-color:rgba(0,0,0,0); padding:0px; color:white; border: none; box-shadow: none;" onclick="window.open('https://clevvur.com', '_blank')">Powered ⚡️by Clevvur.com</button>
+                  <button style="text-align: left; font-size: 12px; text-decoration: underline; background-color:rgba(0,0,0,0); padding:0px; color:white; border: none; box-shadow: none;" onclick="window.open('https://clevvur.com', '_blank')">Powered ⚡️by Clevvur.com</button>
                 </div>
 
             </div>
