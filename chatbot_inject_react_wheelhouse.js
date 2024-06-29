09fdb59356c87d9ce8e5a71386f906e9d1e88e00
Valentin Forestier <81619333+valforest90@users.noreply.github.com>
2024-06-29 08:41:46 +0200

    Update chatbot_inject_react_wheelhouse.js

--- chatbot_inject_react_wheelhouse.js
+++ chatbot_inject_react_wheelhouse.js
@@ -165,7 +165,7 @@
                 right: 0;
                 bottom: 0;
                 width: 500px; 
-                height:85vh;
+                height:65vh;
                 margin-right: 10px;
                 margin-bottom: 10px;
                 background-color: transparent;
