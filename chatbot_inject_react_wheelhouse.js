dab74825ce62ba4257e96e313b741227bdc75890
Valentin Forestier <81619333+valforest90@users.noreply.github.com>
2024-07-09 17:45:56 +0200

    fixed font size

--- chatbot_inject_react_wheelhouse.js
+++ chatbot_inject_react_wheelhouse.js
@@ -46,7 +46,7 @@
                 border-radius: 50px;
                 border-bottom-right-radius: 0px;
                 position: absolute;
-                font-size: calc(100% + 2px);
+                font-size: 16px !important;
                 border: 1px solid black;
                 animation-iteration-count: 5;
               }
@@ -204,7 +204,7 @@
                 border-radius: 50px;
                 border-bottom-right-radius: 0px;
                 position: absolute;
-                font-size: calc(100% + 2px);
+                font-size: 16px !important;
                 border: 1px solid black;
                 animation-iteration-count: 5;
              }
